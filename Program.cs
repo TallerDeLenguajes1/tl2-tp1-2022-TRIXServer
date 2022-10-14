9728213443395fcff32cad57110034934bf55c18
TRIXServer <admin@trixserver.com>
2022-10-14 07:06:25 -0300

    commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+System.Console.Clear();
+System.Console.WriteLine("--\tCalcular cuadrado");
+System.Console.WriteLine();
+System.Console.Write("Ingrese un numero: ");
+
+try
+{
+    int numeroIngresado = int.Parse(Console.ReadLine());
+    System.Console.WriteLine();
+    System.Console.WriteLine($"El cuadrado de {numeroIngresado} es: " + numeroIngresado * numeroIngresado);
+
+}
+catch (System.Exception)
+{
+    System.Console.WriteLine("Numero no valido");
+}
+
