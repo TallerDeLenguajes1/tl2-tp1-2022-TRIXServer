87f2c7ad72a9e314908c95e960c0b433aa882d94
TRIXServer <admin@trixserver.com>
2022-10-14 07:28:16 -0300

    commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+System.Console.Clear();
+System.Console.WriteLine("--\tDividir dos numeros");
+System.Console.WriteLine();
+System.Console.Write("Ingrese el primer numero: ");
+int numeroUno = int.Parse(Console.ReadLine());
+System.Console.WriteLine();
+System.Console.Write("Ingrese el segundo numero: ");
+int numeroDos = int.Parse(Console.ReadLine());
+
+try
+{
+    System.Console.WriteLine();
+    System.Console.WriteLine($"{numeroUno} / {numeroDos} = " + numeroUno / numeroDos);
+
+}
+catch (Exception ex)
+{
+    var msg = "Error message: " + ex.Message;
+    System.Console.WriteLine(msg);
+}
+
